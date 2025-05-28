0894f47eb713b32767a89e71c25aba2a629b8412
David Spear <byter9000@gmail.com>
2025-05-28 15:06:10 -0700

    Add project files.

---
+++ TheNewEra/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TheNewEra.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
