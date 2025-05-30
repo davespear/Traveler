27154be5931cf06ee0c89926f83e45849e17cad6
David Spear <byter9000@gmail.com>
2025-05-30 13:00:44 -0700

    More refactoring of database context.

--- TheNewEra/Data/ApplicationDbContext.cs
+++ TheNewEra/Data/ApplicationDbContext.cs
@@ -22,36 +22,44 @@ public class ApplicationDbContext : IdentityDbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+        
+        MapSmallArmsTables(modelBuilder);
+        SmallArmsSeed(modelBuilder);       
+    }
 
+    // Maps for modelBuilders
+    private static void MapSmallArmsTables(ModelBuilder modelBuilder)
+    {
         modelBuilder.Entity<SmallArmsAmmoTypeDropdownModel>().ToTable("SmallArmsAmmoTypeDropdown");
         modelBuilder.Entity<SmallArmsAmmoTypeModifierDropdownModel>().ToTable("SmallArmsAmmoTypeModifierDropdown");
         modelBuilder.Entity<SmallArmsAmmoTLModifierDropdownModel>().ToTable("SmallArmsTLTypeAmmoDropdown");
         modelBuilder.Entity<SmallArmsAmmoCartridgeTypeModifierDropdownModel>().ToTable("SmallArmsAmmoCartridgeTypeModifierDropdown");
         modelBuilder.Entity<SmallArmsAmmoSpecialModifierDropdownModel>().ToTable("SmallArmsAmmoSpecialModifierDropdown");
+    }
 
+    // Seed data for modelBuilders
+    public static void SmallArmsSeed(ModelBuilder modelBuilder)
+    {
         modelBuilder.Entity<SmallArmsAmmoTypeDropdownModel>().HasData(
             new SmallArmsAmmoTypeDropdownModel { Id = 1, Text = "Straight", Modifier = 0.003 },
             new SmallArmsAmmoTypeDropdownModel { Id = 2, Text = "Necked", Modifier = 0.008 },
             new SmallArmsAmmoTypeDropdownModel { Id = 3, Text = "Shotgun", Modifier = 0.01 }
         );
-
         modelBuilder.Entity<SmallArmsAmmoTypeModifierDropdownModel>().HasData(
            new SmallArmsAmmoTypeModifierDropdownModel { Id = 1, Text = "Shotgun", Modifier = .01 },
            new SmallArmsAmmoTypeModifierDropdownModel { Id = 2, Text = "Mass-produced", Modifier = .02 },
            new SmallArmsAmmoTypeModifierDropdownModel { Id = 3, Text = "Conventional", Modifier = .04 },
            new SmallArmsAmmoTypeModifierDropdownModel { Id = 4, Text = "High-powered", Modifier = .05 }
         );
-
         modelBuilder.Entity<SmallArmsAmmoTLModifierDropdownModel>().HasData(
             new SmallArmsAmmoTLModifierDropdownModel { Id = 1, Text = "3", Modifier = 0.6 },
             new SmallArmsAmmoTLModifierDropdownModel { Id = 2, Text = "4", Modifier = 0.7 },
             new SmallArmsAmmoTLModifierDropdownModel { Id = 3, Text = "5", Modifier = 0.8 },
             new SmallArmsAmmoTLModifierDropdownModel { Id = 4, Text = "6", Modifier = 0.9 },
             new SmallArmsAmmoTLModifierDropdownModel { Id = 5, Text = "7", Modifier = 1.0 },
-            new SmallArmsAmmoTLModifierDropdownModel { Id = 6, Text = "8", Modifier = 1.1 },            
+            new SmallArmsAmmoTLModifierDropdownModel { Id = 6, Text = "8", Modifier = 1.1 },
             new SmallArmsAmmoTLModifierDropdownModel { Id = 7, Text = "9+", Modifier = 1.3 }
         );
-
         modelBuilder.Entity<SmallArmsAmmoCartridgeTypeModifierDropdownModel>().HasData(
            new SmallArmsAmmoCartridgeTypeModifierDropdownModel { Id = 1, Text = "Shotgun", Modifier = .2 },
            new SmallArmsAmmoCartridgeTypeModifierDropdownModel { Id = 2, Text = "Shotgun ETC", Modifier = .3 },
